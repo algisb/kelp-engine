eb0dc0de7ec48bbe8419ad898e60ca82772da9de
algisb <algirdas.balciunas@outlook.com>
2018-01-10 14:35:59 +0000

    new debugging flags added to cmakeLists + vector subscript out of range errors fixed

--- src/component/RenderLine.cpp
+++ src/component/RenderLine.cpp
@@ -31,39 +31,42 @@ void RenderLine::renderLines(Shader * _shader, Camera * _rc)
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
 
-    int _numVertices = m_verticies.size();
+    int numVertices = m_verticies.size();
 
-    //totalVertex = totalVertex + _numVertices;
-    GLuint vbo = 0;
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * _numVertices * 3, &m_verticies[0], GL_DYNAMIC_DRAW);
-
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(0);
+    if(numVertices > 0)
+    {
+        //totalVertex = totalVertex + _numVertices;
+        GLuint vbo = 0;
+        glGenBuffers(1, &vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(float) * numVertices * 3, &m_verticies[0], GL_DYNAMIC_DRAW);
 
 
-    glUseProgram(_shader->m_shaderLocation);
-    kep::Matrix4 m4;
-    glUniformMatrix4fv(_shader->m_shaderModelMatLocation, 1, 
-                       GL_FALSE, &m4.d[0][0]);
-    glUniformMatrix4fv(_shader->m_shaderViewMatLocation, 1, 
-                       GL_FALSE, &_rc->m_viewMat.d[0][0]);
-    glUniformMatrix4fv(_shader->m_shaderProjMatLocation, 1, 
-                       GL_FALSE, &_rc->m_projectionMat.d[0][0]);
-    glUniform3fv(_shader->m_shaderColourLocation, 1, kep::Vector3(0.0f, 1.0f, 0.0f).data);
-    
-    // Tell OpenGL to draw it
-    // Must specify the type of geometry to draw and the number of vertices
-    glDrawArrays(GL_LINES, 0, _numVertices);
-    // Unbind VAO
-    glBindVertexArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glDeleteBuffers(1, &vbo);
-    glDeleteVertexArrays(1, &vao);
-    m_verticies.clear();
-    m_colours.clear();
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+        glEnableVertexAttribArray(0);
+
+
+        glUseProgram(_shader->m_shaderLocation);
+        kep::Matrix4 m4;
+        glUniformMatrix4fv(_shader->m_shaderModelMatLocation, 1, 
+                        GL_FALSE, &m4.d[0][0]);
+        glUniformMatrix4fv(_shader->m_shaderViewMatLocation, 1, 
+                        GL_FALSE, &_rc->m_viewMat.d[0][0]);
+        glUniformMatrix4fv(_shader->m_shaderProjMatLocation, 1, 
+                        GL_FALSE, &_rc->m_projectionMat.d[0][0]);
+        glUniform3fv(_shader->m_shaderColourLocation, 1, kep::Vector3(0.0f, 1.0f, 0.0f).data);
+        
+        // Tell OpenGL to draw it
+        // Must specify the type of geometry to draw and the number of vertices
+        glDrawArrays(GL_LINES, 0, numVertices);
+        // Unbind VAO
+        glBindVertexArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glDeleteBuffers(1, &vbo);
+        glDeleteVertexArrays(1, &vao);
+        m_verticies.clear();
+        m_colours.clear();
+    }
 }
 
 void RenderLine::init()
@@ -113,41 +116,44 @@ void RenderLine2::renderLines(Shader * _shader, Camera * _rc)
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
 
-    int _numVertices = m_verticies.size();
-
-    //totalVertex = totalVertex + _numVertices;
-    GLuint vbo = 0;
-    glGenBuffers(1, &vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * _numVertices * 3, &m_verticies[0], GL_DYNAMIC_DRAW);
-
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-    glEnableVertexAttribArray(0);
-
-
-    glUseProgram(_shader->m_shaderLocation);
-    kep::Matrix4 m4;
-    glUniformMatrix4fv(_shader->m_shaderModelMatLocation, 1, 
-                       GL_FALSE, &m4.d[0][0]);
-    glUniformMatrix4fv(_shader->m_shaderViewMatLocation, 1, 
-                       GL_FALSE, &m4.d[0][0]);//_rc->m_viewMat.d[0][0]
-    glUniformMatrix4fv(_shader->m_shaderProjMatLocation, 1, 
-                       GL_FALSE, &_rc->m_projectionMat.d[0][0]);
-    glUniform3fv(_shader->m_shaderColourLocation, 1, kep::Vector3(1.0f, 0.0f, 0.0f).data);
+    int numVertices = m_verticies.size();
     
-    // Tell OpenGL to draw it
-    // Must specify the type of geometry to draw and the number of vertices
-    glDisable(GL_DEPTH_TEST);
-    glDrawArrays(GL_LINES, 0, _numVertices);
-    glEnable(GL_DEPTH_TEST);
-    // Unbind VAO
-    glBindVertexArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glDeleteBuffers(1, &vbo);
-    glDeleteVertexArrays(1, &vao);
-    m_verticies.clear();
-    m_colours.clear();
+    if(numVertices > 0)
+    {
+        //totalVertex = totalVertex + _numVertices;
+        GLuint vbo = 0;
+        glGenBuffers(1, &vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(float) * numVertices * 3, &m_verticies[0], GL_DYNAMIC_DRAW);
+
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+        glEnableVertexAttribArray(0);
+
+
+        glUseProgram(_shader->m_shaderLocation);
+        kep::Matrix4 m4;
+        glUniformMatrix4fv(_shader->m_shaderModelMatLocation, 1, 
+                        GL_FALSE, &m4.d[0][0]);
+        glUniformMatrix4fv(_shader->m_shaderViewMatLocation, 1, 
+                        GL_FALSE, &m4.d[0][0]);//_rc->m_viewMat.d[0][0]
+        glUniformMatrix4fv(_shader->m_shaderProjMatLocation, 1, 
+                        GL_FALSE, &_rc->m_projectionMat.d[0][0]);
+        glUniform3fv(_shader->m_shaderColourLocation, 1, kep::Vector3(1.0f, 0.0f, 0.0f).data);
+        
+        // Tell OpenGL to draw it
+        // Must specify the type of geometry to draw and the number of vertices
+        glDisable(GL_DEPTH_TEST);
+        glDrawArrays(GL_LINES, 0, numVertices);
+        glEnable(GL_DEPTH_TEST);
+        // Unbind VAO
+        glBindVertexArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glDeleteBuffers(1, &vbo);
+        glDeleteVertexArrays(1, &vao);
+        m_verticies.clear();
+        m_colours.clear();
+    }
 }
 
 void RenderLine2::init()
